48534b6721309b104e1f657d1388c0ca8870a02e
kieran <thekfactor141@gmail.com>
2025-04-08 15:10:29 +0200

    Initial commit

---
+++ get_ids_safe.py
@@ -0,0 +1,120 @@
+import pandas
+import requests
+import re
+import csv
+import json
+
+
+file = pandas.read_csv("all_c_cpp_release2.0.csv")
+
+commit_links = file.ref_link
+changes = file.files_changed
+GITHUB_TOKEN = "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"
+
+def clean_code_line(response,line):
+    """cleans lines of code removing exess whitespace, comments and special characters"""
+    #removes inline comments
+    if line.startswith("*"):
+        pass
+    elif line.startswith('/*'):
+        pass
+    elif line.startswith('//'):
+        pass
+    else:
+        line = re.sub(r"/\*.*?\*/", "", line, flags = re.DOTALL)
+        line = line.strip()
+        return line
+
+#extract the gtihub diffs from the urls in the datbase
+def get_diff(commit_url):
+
+
+    patch_url = commit_url + ".diff"
+    headers = {"Authorization": f"token {GITHUB_TOKEN}"}
+
+    #response = requests.get(patch_url, headers=headers)
+    #diff_content = response.text
+
+    try:
+        response = requests.get(patch_url, headers=headers)
+        diff_content = response.text
+        sequences = extract_lines(diff_content)
+        return sequences
+
+
+    except requests.RequestException as e:
+        print(f"Error fetching diff: {response.status_code}")
+        return None
+   #if response.status_code == 200:
+    #    return diff_content
+   # else:
+    #    print(f"Error fetching diff: {response.status_code}")
+     #   return "\n"
+# Exracts lines of code 
+def extract_lines(response):
+    current_file =None
+    unsafe_operations = []
+
+    for line in response.split("\n"):
+        match = re.match(r"^diff --git a/(.+) b/\1", line)
+        if match:
+            current_file = match.group(1)
+            continue
+        if current_file and re.search(r"README(\.\w+)?$", current_file, re.IGNORECASE):
+            continue
+
+        if line.startswith("---"):
+            pass
+        elif line.startswith("+"): #removed lines
+            clean_line = clean_code_line(response,line[1:].strip())
+            if clean_line:
+                unsafe_operations.append(clean_line)
+
+    return unsafe_operations
+
+
+inc = 0
+def save_to_csv(filename):
+    inc = 0
+    with open(filename, "w", newline="") as file:
+        writer = csv.writer(file)
+        for commit in commit_links:
+            inc += 1
+            diff= get_diff(commit)
+            sequences = extract_lines(diff)
+            writer.writerow(sequences)
+            print(f"{inc} is added")
+
+def save_to_json(filename):
+    inc = 0
+    with open(filename,"w") as file:
+        for commit in commit_links:
+            inc += 1
+            diff = get_diff(commit)
+            if not diff:
+                pass
+                print(f"{inc} is not added")
+            else:
+                json.dump(diff, file, indent =4)
+                print(f"{inc} is added")
+#save_to_csv("unsafe.csv")
+save_to_json("safe.json")
+#inc = 0
+#for commit in commit_links:
+ #   inc += 1
+  #  diff = get_diff(commit)
+   # print(inc)
+
+   #print(extract_lines(diff))
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ split_data.py
@@ -0,0 +1,40 @@
+import json 
+import re
+
+from sklearn.model_selection import train_test_split
+
+def fix_json_file(file_path):
+    with open(file_path, 'r', encoding='utf-8') as file:
+        raw_content =file.read()
+
+    content = "[" + re.sub(r'\]\s*\[', '],[', raw_content.strip()) + "]"
+
+    data = json.loads(content)
+    return data
+
+
+fixed_data = fix_json_file('safe.json')
+
+if fixed_data is not None:
+    with open('safe_fixed.json','w', encoding='utf-8') as fixed_file:
+        json.dump(fixed_data, fixed_file, indent=1)
+        print("fixed")
+        print(len(fixed_data))
+    # data should be a list of sequences
+with open('safe_fixed.json', 'r', encoding='utf-8') as file:
+    data = json.load(file)
+
+train_data, test_data = train_test_split(data, test_size=0.2, random_state= 42)
+
+# Save training data
+with open('safe_operations_train.json', 'w', encoding='utf-8') as train_file:
+    json.dump(train_data, train_file, indent=4)  # Pretty-print JSON
+
+# Save testing data
+with open('safe_operations_test.json', 'w', encoding='utf-8') as test_file:
+    json.dump(test_data, test_file, indent=4)
+
+print("Training and testing JSON files created successfully!")
+
+# Check the size
+
